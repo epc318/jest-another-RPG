7f28a40ba08b0be8161b86a664f57cffc510e16b
Rick Johnson <carl4917@umn.edu>
2021-12-07 21:39:36 -0600

    player test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,8 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
 const Player = require('../lib/Player');
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -6,4 +11,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+      expect.arrayContaining([expect.any(Object)])
+    );
   });
